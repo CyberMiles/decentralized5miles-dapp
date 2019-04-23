aaee2b0de3c23a0c51b2c68e6ca9b1ab3c700202
dragon <longzhi@gmail.com>
2019-04-23 12:54:44 +0800

    styles

--- src/models/Product.js
+++ src/models/Product.js
@@ -27,7 +27,27 @@ class Product {
     // id|userId|status||createdAt|updatedAt, title, desc, price, category, location, imageLink
     const [id, userId, state, createdAt, updatedAt] = rawStrArray[0].split('|');
     const [, title, desc, price, category, location, imageLink] = rawStrArray;
-    const ob = new Product(id, title, desc, price, category, userId, imageLink, location, state, createdAt, updatedAt);
+
+    // handle image link(sample: http://res.5milesapp.com/image/upload/v1555646242/byz7phcp583kxxrsjp3w.webp)
+    const ts = imageLink.match(/v[0-9]{10}/)[0]; // v1555646242
+    const split = imageLink.split(ts);
+    const newImageLink = `${split[0]}f_auto,t_i800/${ts}${split[1]}`
+      .replace('http', 'https')
+      .replace('res.5milesapp.com', 'fivemiles-res.cloudinary.com');
+
+    const ob = new Product(
+      id,
+      title,
+      desc,
+      price,
+      category,
+      userId,
+      newImageLink,
+      location,
+      state,
+      createdAt,
+      updatedAt
+    );
 
     return ob;
   }
